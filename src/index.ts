d13e4f9402789616e2a9b16d2ad3c4e0aa228268
samsostudios <sam@samsostudios.com>
2023-03-10 16:46:04 -0700

    gsap menu

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
+gsap.registerPlugin(SplitText);
+
 window.Webflow ||= [];
 window.Webflow.push(() => {
   console.log('/// karsh hagan - 2023 ///');
+
+  const splitMenu = new SplitText('#menuText', { type: 'words, chars' });
+  const tl = gsap.timeline();
+  tl.to(splitMenu.chars, { duration: 0.6, y: '1rem', stagger: { each: 0.2 } });
 });
