871414d1ffbdd0bef2857910c8f7993d8ff63104
samsostudios <sam@samsostudios.com>
2023-05-15 11:54:50 -0600

    refactored page transition

--- src/components/pageTransition.ts
+++ src/components/pageTransition.ts
@@ -1,34 +1,36 @@
 import { pageTransitionIn, pageTransitionOut } from '$motion/pageTransitionMotion';
 
-export const pageTransition = () => {
-  console.log('PAGE TRANSITON');
-  const allLinks = [...document.querySelectorAll('a')];
+console.log('PAGE TRANSITON');
+const allLinks = [...document.querySelectorAll('a')];
 
-  const pageLocation = window.location.pathname;
-  const hasFurtherIndex = pageLocation.substring(1);
-  if (hasFurtherIndex !== '') {
-    pageTransitionOut();
+const pageLocation = window.location.pathname;
+const hasFurtherIndex = pageLocation.substring(1);
+if (hasFurtherIndex !== '') {
+  pageTransitionOut();
+}
+
+for (const i in allLinks) {
+  const temp = allLinks[i] as HTMLAnchorElement;
+  const linkSrc = temp.href;
+
+  if (linkSrc.includes('karshhagan') && !linkSrc.includes('#')) {
+    temp.addEventListener('click', (e) => {
+      // console.log(temp.href);
+      const animation = pageTransitionIn();
+      animation.play();
+      const animationDuration = animation.duration() * 1000;
+      e.preventDefault();
+      setTimeout(
+        (url: Location) => {
+          window.location = url;
+        },
+        animationDuration,
+        linkSrc
+      );
+    });
   }
+}
 
-  for (const i in allLinks) {
-    const temp = allLinks[i] as HTMLAnchorElement;
-    const linkSrc = temp.href;
+// export const pageTransition = () => {
 
-    if (linkSrc.includes('karshhagan') && !linkSrc.includes('#')) {
-      temp.addEventListener('click', (e) => {
-        // console.log(temp.href);
-        const animation = pageTransitionIn();
-        animation.play();
-        const animationDuration = animation.duration() * 1000;
-        e.preventDefault();
-        setTimeout(
-          (url: Location) => {
-            window.location = url;
-          },
-          animationDuration,
-          linkSrc
-        );
-      });
-    }
-  }
-};
+// };
