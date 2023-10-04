ab5be680bcc27fa544c9bcff2e1b8a37715d0d21
samsostudios <sam@samsostudios.com>
2023-10-03 18:24:25 -0600

    more test for env

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,9 @@ import { editorCheck } from '$utils/editorCheck';
 import { inertiaMovement } from '$utils/intertiaMovement';
 import { setDeepLinks } from '$utils/scrollToDeepLink';
 import { smoothScroll } from '$utils/smoothScroll';
-import type { Webflow } from '@finsweet/ts-utils';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const flow = window.Webflow as Webflow;
-  const fEnv = flow.env();
-  console.log('/// dev ///', flow, fEnv);
-
   const windowLocation = window.location.pathname as string;
 
   // ---------------
--- src/utils/editorCheck.ts
+++ src/utils/editorCheck.ts
@@ -1,4 +1,11 @@
+import type { Webflow } from '@finsweet/ts-utils';
+
 export const editorCheck = () => {
+  if (Webflow.env('editor') === undefined) {
+    console.log('code no editor');
+  } else {
+    console.log('code editor');
+  }
   setTimeout(() => {
     const editorButton = document.querySelector(
       '.w-reset.w-editor-bem-EditSiteButton '
