6e8cb1b3928076670bfbbce839adfdca3050e89d
samsostudios <sam@samsostudios.com>
2024-02-19 15:40:31 -0700

    finalized footer sizing

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -1,25 +1,18 @@
 // import { generateHubspotJSON } from '$utils/generateHubspotJSON';
 import { footerTextMovement } from '$motion/footerMotion';
 import { generateHubspotJSON } from '$utils/generateHubspotJSON';
-import { getDeviceType } from '$utils/getDevice';
 import { postContactData } from '$utils/postContactForm';
 
 // import { gsap } from 'gsap';
 
 export const footer = () => {
+  // Footer Size Adjustments
+  // --------------------
   const footerSpacer = document.querySelector('.section_footer-spacer') as HTMLElement;
   const footer = document.querySelector('.footer_component') as HTMLElement;
   const footerHeight = parseInt(getComputedStyle(footer).height.split('px')[0]) - 64;
 
-÷  window.addEventListener('resize', () => {
-    const footerHeight = parseInt(getComputedStyle(footer).height.split('px')[0]) - 64;
-
-    footerSpacer.style.height = footerHeight + 'px';
-  });
-
-  // gsap.to(footerSpacer, { height: footerHeight, duration: 0.5, delay: 5 });
-
-  console.log(footerHeight);
+  footerSpacer.style.height = footerHeight + 'px';
 
   // Footer Text Movement
   // --------------------
