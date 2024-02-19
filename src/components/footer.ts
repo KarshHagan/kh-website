5c8790e5b57a8c9046d4dbd9bc7c12b9338573e0
samsostudios <sam@samsostudios.com>
2024-02-19 14:54:59 -0700

    footer progress

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -4,7 +4,23 @@ import { generateHubspotJSON } from '$utils/generateHubspotJSON';
 import { getDeviceType } from '$utils/getDevice';
 import { postContactData } from '$utils/postContactForm';
 
+// import { gsap } from 'gsap';
+
 export const footer = () => {
+  const footerSpacer = document.querySelector('.section_footer-spacer') as HTMLElement;
+  const footer = document.querySelector('.footer_component') as HTMLElement;
+  const footerHeight = parseInt(getComputedStyle(footer).height.split('px')[0]) - 64;
+
+÷  window.addEventListener('resize', () => {
+    const footerHeight = parseInt(getComputedStyle(footer).height.split('px')[0]) - 64;
+
+    footerSpacer.style.height = footerHeight + 'px';
+  });
+
+  // gsap.to(footerSpacer, { height: footerHeight, duration: 0.5, delay: 5 });
+
+  console.log(footerHeight);
+
   // Footer Text Movement
   // --------------------
   footerTextMovement();
--- src/motion/footerMotion.ts
+++ src/motion/footerMotion.ts
@@ -3,9 +3,9 @@ import { gsap } from 'gsap';
 export const footerTextMovement = () => {
   const footerComponent = document.querySelector('.footer_component') as HTMLElement;
   const footerTextWrap = footerComponent.querySelector('.footer-overflow-wrap') as HTMLElement;
-  const footerText = footerComponent.querySelector('.footer_large-text') as HTMLElement;
 
-  const textWidth = footerText.scrollWidth;
+  const textWidth = footerTextWrap.scrollWidth;
+  const computedMovement = textWidth - textWidth / 3;
 
   const textSpeed = 45;
   const animation = gsap.timeline({
@@ -13,9 +13,9 @@ export const footerTextMovement = () => {
       animation.restart();
     },
   });
-  animation.to(footerText, {
+  animation.to(footerTextWrap, {
     duration: textSpeed,
-    x: -textWidth,
+    x: -computedMovement,
     ease: 'linear',
   });
 };
