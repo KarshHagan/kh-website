3121857dc6ab0410644562f3e83649e9a746777f
samsostudios <sam@samsostudios.com>
2024-05-23 17:12:36 -0600

    careers feed updated

--- src/components/careersFeed.ts
+++ src/components/careersFeed.ts
@@ -4,17 +4,19 @@ import { cloneNode } from '@finsweet/ts-utils';
 
 export const careersFeed = () => {
   setTimeout(() => {
-    const careersList = document.querySelector('.careers-list_feed') as HTMLElement;
-    const bambooContainer = careersList.querySelector('#BambooHR') as HTMLElement;
-    const jobsContainer = bambooContainer.querySelector('.BambooHR-ATS-Jobs-List') as HTMLElement;
-    const jobsList = [...jobsContainer.querySelectorAll('li')];
+    const bambooContainer = document.querySelector('#BambooHR') as HTMLElement;
 
-    jobsList.forEach((item) => {
-      createJob(item);
-    });
+    bambooContainer &&
+      (() => {
+        const jobsList = [...document.querySelectorAll('.BambooHR-ATS-Jobs-Item')];
+
+        jobsList.forEach((item) => {
+          createJob(item);
+        });
+      })();
   }, 1500);
 
-  function createJob(data: HTMLElement) {
+  function createJob(data: Element) {
     const jobList = document.querySelector('.careers-list_list') as HTMLElement;
     const jobTemplate = document.querySelector('.careers-list_item') as HTMLElement;
     const newJob = cloneNode(jobTemplate) as HTMLElement;
@@ -23,10 +25,11 @@ export const careersFeed = () => {
 
     const titleData = data.querySelector('a') as HTMLAnchorElement;
     const locationData = data.querySelector('span') as HTMLElement;
+
     const setLink = newJob.querySelector('a') as HTMLAnchorElement;
     const setTitle = newJob.querySelector('h3') as HTMLElement;
     const setLocation = newJob.querySelector('.careers-list_location') as HTMLElement;
-    const formatLocation = locationData.innerText.split('\n')[2].split('\t')[19];
+    const formatLocation = locationData.innerText.split('\n')[2];
 
     setLink.href = titleData.href;
     setTitle.innerText = titleData.innerText;
