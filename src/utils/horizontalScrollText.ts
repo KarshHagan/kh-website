8ae2aa515c26c8a98dd11cb958596f0b002c87ea
samsostudios <sam@samsostudios.com>
2025-01-23 12:48:56 -0700

    variant update to scroll text module - testing release

--- src/utils/horizontalScrollText.ts
+++ src/utils/horizontalScrollText.ts
@@ -11,23 +11,28 @@ export const horizontalScrollText = () => {
   const scroll = scrollSections[0].scrollWidth;
   const windowWidth = window.innerWidth;
 
-  console.log('!!!!');
-
-  let sp = '25%, 100%';
-  let ep = '100% 100%';
-
   const bp = breakpoints()[0];
-
-  if (bp === 'mobile-landscape' || bp === 'mobile-portrait') {
-    sp = '10% 100%';
-    ep = '60% 100%';
-  }
 
   const calcWidth = scroll - windowWidth;
 
   for (const i in scrollSections) {
     const tempSection = scrollSections[i] as HTMLElement;
     const textElement = [...tempSection.querySelectorAll('[data-scroll-text]')];
+    const scrollType = tempSection.dataset.scrollTextWrap;
+
+    // Default
+    let sp = '25%, 100%';
+    let ep = '100% 100%';
+
+    if (bp === 'mobile-landscape' || bp === 'mobile-portrait') {
+      sp = '10% 100%';
+      ep = '60% 100%';
+    }
+
+    if (scrollType === 'top') {
+      sp = '0% 100%';
+      ep = '100% 100%';
+    }
 
     const animation = gsap.timeline({
       scrollTrigger: {
